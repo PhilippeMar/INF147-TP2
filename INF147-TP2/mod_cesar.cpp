6c7cdcb75e5d037ff881d7af11e5ae6b0ec990f6
TeanoRocha <113925520+TeanoRocha@users.noreply.github.com>
2022-11-04 05:31:58 -0400

    Update mod_cesar.cpp

--- INF147-TP2/mod_cesar.cpp
+++ INF147-TP2/mod_cesar.cpp
@@ -20,21 +20,50 @@ On y retrouve les sous-programmes suivants :
 // Genere une cle pour un decalage donne.
 void CESAR_generer_cle(unsigned char* cle, const int decalage)
 {
-	// TODO
+	int n = fabs(decalage % TAILLE_ALPHABET);
+	
+	char temporaire[TAILLE_ALPHABET];
+
+	for (int a = ASCII_a; a <= ASCII_z; a++)
+	{
+		temporaire[a - ASCII_a] = a;
+	}
+
+	for (int i = n; i < TAILLE_ALPHABET; i++)
+	{
+		cle[i - n] = temporaire[i];
+	}
+
+	for (int j = TAILLE_ALPHABET - n; j < TAILLE_ALPHABET; j++)
+	{
+		cle[j] = temporaire[j - (TAILLE_ALPHABET - n)];
+	}
 }
 
 // Encode une chaine de caracteres avec le code de Cesar.
-void CESAR_encrypter(unsigned char* message,
-					 const unsigned int taille,
-					 const int decalage)
+void CESAR_encrypter(unsigned char* message, const unsigned int taille, const int decalage)
 {
-	// TODO
+	unsigned char cle_crypte[TAILLE_ALPHABET];
+
+	CESAR_generer_cle(cle_crypte,decalage);
+
+	int i = 0;
+	while (i < taille)
+	{
+		if (isalpha(message[i]) != 0)
+		{
+			message[i] = cle_crypte[message[i] - ASCII_a];
+		}
+		i++;
+	}
+
 }
 
 // Decode une chaine de caracteres avec le code de Cesar.
-void CESAR_decrypter(unsigned char* message,
-					 const unsigned int taille,
-					 const int decalage)
+void CESAR_decrypter(unsigned char* message, const unsigned int taille, const int decalage)
 {
-	// TODO
+	int oppose_decalage = decalage * -1;
+
+	CESAR_encrypter(message, taille, oppose_decalage);
+
 }
