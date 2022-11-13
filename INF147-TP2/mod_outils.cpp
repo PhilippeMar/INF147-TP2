95474e0249a5408333b1fef7d59788ab5a5bb8f5
jpportant <113920371+jpportant@users.noreply.github.com>
2022-11-13 17:09:52 -0500

    ajout de commentaire

--- INF147-TP2/mod_outils.cpp
+++ INF147-TP2/mod_outils.cpp
@@ -6,9 +6,15 @@ Auteurs :	Barry Lawson
 Date : Octobre 2022
 
 --Description
+	Ce module contient des outils utiliser pour les divers types d'encryption
 
 On y retrouve les sous-programmes suivants :
-	-
+	- OUTILS_afficher_tableau_1D_uchar
+	- OUTILS_obtenir_position_max_dbl
+	- test_OUTILS_obtenir_position_max_dbl
+	- OUTILS_melanger
+	- OUTILS_tri_decroissant_etendu_dbl_uchar
+	- test_OUTILS_tri_decroissant_etendu_dbl_uchar
 *****************************************************************************************/
 
 #include "mod_outils.h"
@@ -19,17 +25,28 @@ On y retrouve les sous-programmes suivants :
 
 // Affiche un talbeau 1D de caracteres.
 void OUTILS_afficher_tableau_1D_uchar(unsigned char* tableau,
-									  const unsigned int taille_tableau)
+	const unsigned int taille_tableau)
 {
-	// TODO
+	for (unsigned int i = 0; i < taille_tableau; i++)
+		printf("%c ", tableau[i]);
+	printf("\n");
 }
 
 // Trouve l'indice de la plus grande valeur dans un tableau de rationnels.
 unsigned int OUTILS_obtenir_position_max_dbl(const double* tableau,
-											 const unsigned int taille)
+											const unsigned int taille)
 {
-	return 0;// Remove this
-	// TODO
+	unsigned int indice = 0;
+	// Parcourir les valeur du tableau
+	for (unsigned int i = 1; i < taille; i++)
+	{
+		// On change la valeur de l'indice si la valeur courante est plus grande
+		if (tableau[i] > tableau[indice])
+		{
+			indice = i;
+		}
+	}
+	return indice;
 }
 
 // Procedure de test pour OUTILS_obtenir_position_max_dbl.
@@ -38,12 +55,12 @@ void test_OUTILS_obtenir_position_max_dbl(void)
 
 	printf("TEST_OUTILS_OBTENIR_POSITION_MAX_DBL :: [-1., 1., 10., 0.]\n\n");
 
-	double tableau[] = {-1., 1., 10., 0.};
+	double tableau[] = { -1., 1., 10., 0. };
 	unsigned int taille = sizeof(tableau) / sizeof(double);
 
 	unsigned int valeur_attendue = 2;
 	unsigned int valeur_obtenue = OUTILS_obtenir_position_max_dbl(tableau, taille);
-	
+
 	printf("\tValeur attendue : %u\n", valeur_attendue);
 	printf("\tValeur obtenue  : %u\n", valeur_obtenue);
 
@@ -52,15 +69,31 @@ void test_OUTILS_obtenir_position_max_dbl(void)
 // Cree une permutation aleatoire d'un tableau 1D avec l'algorithme de Fisher-Yates.
 void OUTILS_melanger(unsigned char* tableau, const unsigned int taille)
 {
-	// TODO
+	for (unsigned int i = taille - 1; i >= 1; i--)
+	{
+		// Assignation d'une valeur aléatiore 
+		unsigned int j = RANDBETWEEN(0, i);
+		// Permutation entre la valeur courante et la valeur aléatoire
+		SWAP_INT(tableau[i], tableau[j]);
+	}
 }
 
 // Trie deux tableaux en etendant le tri du premier aux elements du deuxieme.
 void OUTILS_tri_decroissant_etendu_dbl_uchar(double* tableau_principal,
-											 unsigned char* tableau_secondaire,
-											 const unsigned int taille_tableaux)
+	unsigned char* tableau_secondaire,
+	const unsigned int taille_tableaux)
 {
-	// TODO
+	for (unsigned int i = taille_tableaux; i >= 1; i--)
+	{
+		for (unsigned int j = 0; j < taille_tableaux - 1; j++)
+		{
+			if (tableau_principal[j + 1] > tableau_principal[j])
+			{
+				SWAP_DBL(tableau_principal[j], tableau_principal[j + 1]);
+				SWAP_DBL(tableau_secondaire[j], tableau_secondaire[j + 1]);
+			}
+		}
+	}
 }
 
 // Procedure de test pour OUTILS_tri_decroissant_etendu_dbl_uchar.
@@ -75,22 +108,22 @@ void test_OUTILS_tri_decroissant_etendu_dbl_uchar(void)
 	unsigned int taille_tableaux = sizeof(tableau_principal) / sizeof(double);
 
 	OUTILS_tri_decroissant_etendu_dbl_uchar(tableau_principal,
-											tableau_secondaire,
-											taille_tableaux);
+		tableau_secondaire,
+		taille_tableaux);
 
 	printf("\tValeur attendue : [0.07278, 0.02848, 0.01582, 0.00000]\n");
 	printf("\t                  [98     , 97     , 99     , 100    ]\n\n");
-	
-	printf("\tValeur obenue :   [%.5lf, %.5lf, %.5lf, %.5lf]\n", 
-			tableau_principal[0],
-			tableau_principal[1],
-			tableau_principal[2],
-			tableau_principal[3]);
-	
+
+	printf("\tValeur obenue :   [%.5lf, %.5lf, %.5lf, %.5lf]\n",
+		tableau_principal[0],
+		tableau_principal[1],
+		tableau_principal[2],
+		tableau_principal[3]);
+
 	printf("\t                  [%u     , %u     , %u     , %u    ]\n",
-			tableau_secondaire[0],
-			tableau_secondaire[1],
-			tableau_secondaire[2],
-			tableau_secondaire[3]);
+		tableau_secondaire[0],
+		tableau_secondaire[1],
+		tableau_secondaire[2],
+		tableau_secondaire[3]);
 
 }
