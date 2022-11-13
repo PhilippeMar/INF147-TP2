d45690ea37e9050fd0524b58311da3189e448116
PhilippeM <philippe.marineau@hotmail.com>
2022-11-13 02:45:10 -0500

    mod_cesar complete

--- INF147-TP2/mod_cesar.cpp
+++ INF147-TP2/mod_cesar.cpp
@@ -6,9 +6,12 @@ Auteurs :	Barry Lawson
 Date : Octobre 2022
 
 --Description
+	Ce fichier permet d'encypter et decrypter des message selon le code de Cesar
 
 On y retrouve les sous-programmes suivants :
-	-
+	- CESAR_generer_cle
+	- CESAR_encrypter
+	- CESAR_decrypter
 *****************************************************************************************/
 
 #include "mod_cesar.h"
@@ -20,7 +23,15 @@ On y retrouve les sous-programmes suivants :
 // Genere une cle pour un decalage donne.
 void CESAR_generer_cle(unsigned char* cle, const int decalage)
 {
-	// TODO
+	// On tronque le decalage
+	int n = decalage % TAILLE_ALPHABET;
+	// On veux un decalage positif (-2 => 24)
+	if (n < 0) n += TAILLE_ALPHABET;
+	// Genere la cle
+	for (int i = 0; i < TAILLE_ALPHABET; i++)
+	{
+		cle[i] = ASCII_a + ((i + n) % TAILLE_ALPHABET);
+	}
 }
 
 // Encode une chaine de caracteres avec le code de Cesar.
@@ -28,7 +39,16 @@ void CESAR_encrypter(unsigned char* message,
 					 const unsigned int taille,
 					 const int decalage)
 {
-	// TODO
+	// On initialise la clé d'encryptage
+	unsigned char cle[TAILLE_ALPHABET];
+	CESAR_generer_cle(cle, decalage);
+
+	// On encrypte chaque charactere(a-z) du message
+	for (unsigned int i = 0; i < taille; i++)
+	{
+		if (isalpha(message[i])) // On assume qu'il n'y a pas de majuscule
+			message[i] = cle[message[i] - ASCII_a];
+	}
 }
 
 // Decode une chaine de caracteres avec le code de Cesar.
@@ -36,5 +56,6 @@ void CESAR_decrypter(unsigned char* message,
 					 const unsigned int taille,
 					 const int decalage)
 {
-	// TODO
+	// On rappel la fonction pour encrypter mais avec un decalage dans le sens inverse
+	CESAR_encrypter(message, taille, -decalage);
 }
